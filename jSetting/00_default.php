8e7060f730f6368ff2b5675df20920968a2943f3
vongola12324 <vongola12324@coder.tw>
2013-11-07 20:05:17 +0800

    Upload Setting Files
    It's just an empty file!

---
+++ jSetting/00_default.php
@@ -0,0 +1,5 @@
+// Default Settings
+//
+// Almost Setting can be changed in this file.
+// If you want to enable some function which have setting files in the same directory,
+// Make sure you have enabled the corresponding options.
---
+++ jSetting/01_database.php
@@ -0,0 +1,5 @@
+// DataBase Settings
+//
+// If you want to use SQL, Please enable it in 00_default.php!
+// Here are some SQL setting.
+
