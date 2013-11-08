029c7ee031962e0cd3a8ed556c807d9bccc051fc
火文狼 <vongola12324@coder.tw>
2013-11-08 14:49:39 +0800

    Update 00_default.php

--- jSetting/00_default.php
+++ jSetting/00_default.php
@@ -1,5 +1,23 @@
+<?php
 // Default Settings
 //
 // Almost Setting can be changed in this file.
 // If you want to enable some function which have setting files in the same directory,
 // Make sure you have enabled the corresponding options.
+
+// Set Path & URL
+define('OJ_URL', 'http://localhost');
+define('OJ_PATH', dirname(__FILE__).'/..');
+
+// Set Title
+define('OJ_Title', 'Interactive Online Judge System');
+define('OJ_Subtitle', '');
+
+// Set Default Admin Info
+define('OJ_Admin', 'sysop');
+define('OJ_AdminMail', 'sysop@localhost');
+
+// Set Session Name
+define('OJ_Session_Name', 'IOJ_Session');
+
+?>
